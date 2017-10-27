c633cff6e697aa84a8c16f435bdf3bd84050b1f9
markjconlon <markjconlon@gmail.com>
2017-10-27 14:41:03 -0400

    importing patientData from the json document provided, and Patients component which we provide it with the an array of all of the patients data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Patients from './Patients';
+import patientData from './patientData';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      patients: {patientData}
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      {Object
+        .keys(this.state.patients)
+        .map( key => <Patients key={key} details={this.state.patients[key]}/>)
+      }
       </div>
     );
   }
